624f07b2e7d363b83ce992be4ab88edc65f4e690
Chris Cuomo <weatherfields@protonmail.com>
2019-10-17 17:44:08 -0400

    Timer sorta works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,86 +1,174 @@
 $(document).ready(function() {
 
 // Questions 
-let questionOne = {
-prompt: "In 1991, this bands debut album unseated “The King of Pop”, Michael Jackson from the No. 1 position on the Billboard 200.",
-options: ["<div class='text-center btn btn-info btn-block' data-correct='false'>Soundgarden</div>",  
-"<div class='text-center btn btn-info btn-block' data-correct='true'>Nirvana</div>",
-"<div class='text-center btn btn-info btn-block' data-correct='false'>The Smashing Pumpkins</div>", 
-"<div class='text-center btn btn-info btn-block' data-correct='false'>Red Hot Chili Peppers</div>",  
-"<div class='text-center btn btn-info btn-block' data-correct='false'>Pearl Jam</div>",] 
-}
-let questionTwo = {
-prompt: "Although this English act formed in the late 70’s, they didn’t have a top ten US hit until their 1989 album, Disintegration.",
-options: ["<div class='text-center btn btn-info btn-block' data-correct='false'>Depeche Mode</div>", 
-"<div class='text-center btn btn-info btn-block' data-correct='true'>The Cure</div>",
-"<div class='text-center btn btn-info btn-block' data-correct='false'>Joy Division</div>",
-"<div class='text-center btn btn-info btn-block' data-correct='false'>New Order</div>", 
-"<div class='text-center btn btn-info btn-block' data-correct='false'>Siouxsie and the Banshees</div>",]
-}
-let questionThree = {
+const questions=[{
+    name:"questionOne",
+    prompt: "In 1991, this bands debut album unseated “The King of Pop”, Michael Jackson from the No. 1 position on the Billboard 200.",
+    ansOptions: ["Soundgarden",  
+    "The Smashing Pumpkins", 
+    "Red Hot Chili Peppers",  
+    "Pearl Jam", "Nirvana"],
+    right: "Nirvana",
+},
+{
+    name:"questionTwo",
+    prompt: "Although this English act formed in the late 70’s, they didn’t have a top ten US hit until their 1989 album, Disintegration.",
+    ansOptions: ["Depeche Mode", 
+    "Joy Division",
+    "New Order", 
+    "Siouxsie and the Banshees", "The Cure"],
+    right:"The Cure"
+},
+{   
+    name:"questionThree",
     prompt: "One of the first 90’s alternative acts, this American act experienced commercial success throughout the entire decade. Their first album was called “Gish”.",
-    options: ["<div class='text-center btn btn-info btn-block' data-correct='false'>Alice in Chains</div>", 
-    "<div class='text-center btn btn-info btn-block' data-correct='true'>The Smashing Pumpkins</div>",
-    "<div class='text-center btn btn-info btn-block' data-correct='false'>Pearl Jam</div>",
-    "<div class='text-center btn btn-info btn-block' data-correct='false'>Weezer</div>",
-    "<div class='text-center btn btn-info btn-block' data-correct='false'>Stone Temple Pilots</div>",]
-}
-let questionFour = {
-    prompt: "Though not exaclty an Alternative act, this band had a big influence on many 90’s alternative and indie bands. Their biggest international hit was not sung by the lead singer, but rather the bassist.",
-    options: ["<div class='text-center btn btn-info btn-block' data-correct='true'>The Cars</div>",
-    "<div class='text-center btn btn-info btn-block' data-correct='false'>Cheap Trick</div>",
-    "<div class='text-center btn btn-info btn-block' data-correct='false'>Devo</div>",
-    "<div class='text-center btn btn-info btn-block' data-correct='false'>Aerosmith</div>",
-    "<div class='text-center btn btn-info btn-block' data-correct='false'>The Ramones</div>",]
-}
-let questionFive = {
+    ansOptions: ["Alice in Chains", 
+    "Pearl Jam",
+    "Weezer",
+    "Stone Temple Pilots"],
+    right: "The Smashing Pumpkins"
+},
+{   name:"questionFour",
+    prompt: "Though not exactly an Alternative act, this band had a big influence on many 90’s alternative and indie bands. Their biggest international hit was not sung by the lead singer, but rather the bassist.",
+    ansOptions: ["The Cars",
+    "Cheap Trick",
+    "Devo",
+    "Aerosmith",
+    "The Ramones"],
+    right: "The Cars"
+},
+{   name:"questionFive",
     prompt: "Though not their biggest hit, this bands only MTV Video Music Award was for Best Group Video in 1987 for their song Wild Wild Life?",
-    options: ["<div class='text-center btn btn-info btn-block' data-correct='false'>The Cocteau Twins</div>",
-    "<div class='text-center btn btn-info btn-block' data-correct='false'>Television</div>",
-    "<div class='text-center btn btn-info btn-block' data-correct='false'>Roxy Music</div>",
-    "<div class='text-center btn btn-info btn-block' data-correct='true'>Talking Headsh</div>",
-    "<div class='text-center btn btn-info btn-block' data-correct='false'>Tom Tom Club</div>",]
-}
-let questionSix = {
+    ansOptions: ["The Cocteau Twins",
+    "Television",
+    "Roxy Music",
+    "Talking Heads",
+    "Tom Tom Club"],
+    right: "Talking Heads"
+},
+{   
+    name:"questionSix",
     prompt: "Kurt Cobain admitted to basically copying the loud - soft dynamic that Black Francis and crew had perfected years before Smells Like Teen Spirit was a monster hit. Which influential early alternative band was he referring to?",
-    options: ["<div class='text-center btn btn-info btn-block' data-correct='false'> The Velvet Underground</div>",
-    "<div class='text-center btn btn-info btn-block' data-correct='true'>The Pixies</div>",
-    "<div class='text-center btn btn-info btn-block' data-correct='false'>Sonic Youth</div>",
-    "<div class='text-center btn btn-info btn-block' data-correct='false'>Husker Du</div>",
-    "<div class='text-center btn btn-info btn-block' data-correct='false'>Black Flag</div>",]
+    ansOptions: [
+    "The Velvet Underground",
+    "The Pixies",
+    "Sonic Youth",
+    "Husker Du",
+    "Black Flag"],
+    right: "The Pixies"  
+}]
+
+//render question on page dynamically
+
+
+function showQuestions(){
+
+    var rando = Math.floor(Math.random() * 5 + 1)
     
+    var questionContainer = $('#questions');
+    var questionDiv = $('<div>');
+    questionDiv.attr('id', questions[rando].name);
+   
+    questionDiv.text(questions[rando].prompt);
+    // questionDiv.css('border','solid 2px red');
+    // questionDiv.css('display','block');
+    // questionDiv.css('box-sizing','border-box');
+    // questionDiv.css('height','100px');
+    questionContainer.append(questionDiv);
+
+
+    /*let answerDiv = $('<div>');
+    answerDiv.text(question.right);
+    answerDiv.css('border','solid 2px blue');
+    answerDiv.css('display','block');
+    answerDiv.css('box-sizing','border-box');
+    answerDiv.css('height','50px');
+    answerDiv.attr('data-correct','true');*/
+
+
+    // var wrongAnswers= question[i].wrong;
+
+    // for(var j =0 ; j < wrongAnswers.length; j++) {
+       
+    // let answerDiv = $('<div>');
+    //     answerDiv.text(wrongAnswers[j]);
+    //     answerDiv.css('border','solid 2px blue');
+    //     answerDiv.css('display','block');
+    //     answerDiv.css('box-sizing','border-box');
+    //     answerDiv.css('height','50px');
+    //     answerDiv.attr('data-correct','false');
+
+
+
+    //     questionContainer.append(answerDiv);
+
+
+    // }
+
+
 }
+// Start The Game //
+$(".st-btn").click(function() {
+    $(".st-btn").remove();
+    showQuestions();
+    //$("#questions").css('display', 'inherit');
+    startTimer();
+    
+    function startTimer() {
+        
+        var timer = 12;
+        var id = setInterval(function(){ 
+            $("#printedtimer").text("Time remaining: " + timer + " seconds");
+            timer--; //console.log(secs);
+            if(timer === 0){
+            clearInterval(id);
+            alert('You' + '\'re' + ' out of time!');
+           }
+        }, 1000);
+    }
+        // if (secs < 0) {
+        //     clearInterval(id);
+        //     document.getElementById("#printedtimer").innerHTML = "You're Out Of Time!";
+        //   }
+        // }
+        //DOM manipulation code to throw secs on screen would probably go here
+
+});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Variables Outside Jquery start
-let allQuestions = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix];
+/*let allQuestions = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix];
 let questLength = allQuestions.length;
 let nextQuest = 0;
 let right = 0;
 let wrong = 0;
 let unanswered = 0;
 let countDown = 12;
-let intervalId;
-
-// Start The Game //
-$(".st-btn").click(function() {
-    $(".st-btn").remove();
-    showQuestions(AllQuestions[nextQuest]);
-    $("#questions").css('display', 'inherit');
-    
-});
+let intervalId;*/
 
     // function showQuestions = 
 // Game Starts
 // Timer 
-{
-    let secs = 0;
-    let id = setInterval(function(){ 
-        secs++; //console.log(secs);
-      if(secs> 10){
-        clearInterval(id);
-        alert('You' + '\'re' + ' out of time!');
-       }
-    }, 1000);
-}
+
 // //Functions
 // function correct() {
 //     let rightAnswer = $(this).data("correct");
@@ -95,4 +183,3 @@ $(".st-btn").click(function() {
 // 5 possible answers for question
 // Answers are logged into Right, Wrong, Unanswered as the game proceeds
 // After all questions are asked, the "total-screen" appears where the player sees how they did, and are given an option to restart.
-});
